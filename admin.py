e48ef937e479087fd5fffe9435ce6ef5b47517b1
viniyan <86854657+viniyan@users.noreply.github.com>
2022-02-12 10:06:37 -0300

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import DiasVisita,Cidade,Imagem,Horario,Imovei,Visitas
+
+admin.site.register(DiasVisita)
+admin.site.register(Cidade)
+admin.site.register(Imovei)
+admin.site.register(Imagem)
+admin.site.register(Horario)
+admin.site.register(Visitas)
